139bb44d204c0a0094bdecaa5262ab7f0fac5e98
Michael Snoyman <michael@snoyman.com>
2010-09-15 17:45:04 +0200

    Pool uses TVar instead of MVar

--- Database/Persist/Pool.hs
+++ Database/Persist/Pool.hs
@@ -7,7 +7,9 @@ module Database.Persist.Pool
     , Pool
     ) where
 
-import Control.Concurrent.MVar hiding (modifyMVar, modifyMVar_)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TVar
+    (TVar, newTVarIO, readTVar, writeTVar, readTVarIO)
 import Control.Exception (throwIO)
 import Data.Typeable
 import "MonadCatchIO-transformers" Control.Monad.CatchIO
@@ -21,19 +23,17 @@ data PoolData a = PoolData
 
 data Pool a = Pool
     { poolMax :: Int
-    , poolData :: MVar (PoolData a)
+    , poolData :: TVar (PoolData a)
     , poolMake :: IO a
     }
 
 createPool :: MonadCatchIO m
            => IO a -> (a -> IO ()) -> Int -> (Pool a -> m b) -> m b
 createPool mk fr mx f = do
-    pd <- liftIO $ newMVar $ PoolData [] 0
-    finally (f $ Pool mx pd mk) $ do
-        mress <- liftIO $ tryTakeMVar pd
-        case mress of
-            Nothing -> return ()
-            Just (PoolData ress _) -> liftIO $ mapM_ fr ress
+    pd <- liftIO $ newTVarIO $ PoolData [] 0
+    finally (f $ Pool mx pd mk) $ liftIO $ do
+        PoolData ress _ <- readTVarIO pd
+        mapM_ fr ress
 
 data PoolExhaustedException = PoolExhaustedException
     deriving (Show, Typeable)
@@ -48,10 +48,14 @@ withPool' p f = do
 
 withPool :: MonadCatchIO m => Pool a -> (a -> m b) -> m (Maybe b)
 withPool p f = block $ do
-    eres <- modifyMVar (poolData p) $ \pd -> do
-        case poolAvail pd of
-            (x:xs) -> return (pd { poolAvail = xs }, Right x)
-            [] -> return (pd, Left $ poolCreated pd)
+    eres <- liftIO $ atomically $ do
+        pd <- readTVar $ poolData p
+        let (pd', eres) =
+                case poolAvail pd of
+                    (x:xs) -> (pd { poolAvail = xs }, Right x)
+                    [] -> (pd, Left $ poolCreated pd)
+        writeTVar (poolData p) pd'
+        return eres
     case eres of
         Left pc ->
             if pc >= poolMax p
@@ -64,22 +68,9 @@ withPool p f = block $ do
                         (liftM Just $ unblock $ f res)
                         (insertResource 0 res)
   where
-    insertResource i x = modifyMVar_ (poolData p) $ \pd ->
-        return pd { poolAvail = x : poolAvail pd
-                  , poolCreated = i + poolCreated pd
-                  }
-
-modifyMVar :: MonadCatchIO m => MVar a -> (a -> m (a,b)) -> m b
-modifyMVar m io =
-  block $ do
-    a      <- liftIO $ takeMVar m
-    (a',b) <- unblock (io a) `onException` liftIO (putMVar m a)
-    liftIO $ putMVar m a'
-    return b
-
-modifyMVar_ :: MonadCatchIO m => MVar a -> (a -> m a) -> m ()
-modifyMVar_ m io =
-  block $ do
-    a  <- liftIO $ takeMVar m
-    a' <- unblock (io a) `onException` liftIO (putMVar m a)
-    liftIO $ putMVar m a'
+    insertResource i x = liftIO $ atomically $ do
+        pd <- readTVar $ poolData p
+        writeTVar (poolData p)
+            pd { poolAvail = x : poolAvail pd
+               , poolCreated = i + poolCreated pd
+               }
