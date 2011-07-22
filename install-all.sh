de296e8bab4af8474c2ee7cfd19b3560fcbd4324
Michael Snoyman <michael@snoyman.com>
2011-07-22 17:03:09 +0300

    install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PACKAGES="persistent persistent-sqlite persistent-postgresql persistent-mongoDB persistent-template"
+CABAL=cabal
+
+for package in $PACKAGES
+do
+    echo Installing $package
+    cd $package
+    $CABAL configure --enable-tests && $CABAL build && $CABAL test && ./Setup.lhs install || exit
+    cd ..
+done
