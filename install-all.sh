ca2739d4caa5357f898ddbcd1efaf76774eec1bc
Greg Weber <greg@gregweber.info>
2011-08-11 09:45:03 -0700

    fix testing dependencies for ./install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 
-PACKAGES="pool persistent persistent-template persistent-sqlite persistent-postgresql persistent-mongoDB"
 CABAL=cabal
+
+# also pool and persistent
+PACKAGES="persistent-template persistent-sqlite persistent-postgresql persistent-mongoDB"
+
+cabal_install() {
+    if [ "$2" == "" ]
+    then
+      configure_opts="--enable-tests"
+      test="$CABAL test"
+    else
+      configure_opts=$2
+      test=""
+    fi
+    echo Installing $1
+    cd $1
+    ($CABAL configure $configure_opts ||
+      ($CABAL update && $CABAL install HUnit QuickCheck hspec file-location-0.2.3 && $CABAL install --only-dependencies && $CABAL configure $configure_opts)
+    ) && $CABAL build && $test && ./Setup.lhs install || exit
+    cd ..
+}
+
+# required for persistent
+cabal_install "pool"
+# don't run the persistent tests right now
+cabal_install "persistent" " "
 
 for package in $PACKAGES
 do
-    echo Installing $package
-    cd $package
-    ($CABAL configure --enable-tests ||
-      ($CABAL install --only-dependencies --enable-tests && $CABAL configure --enable-tests)
-    ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit
-    cd ..
+  cabal_install $package
 done
+
+# now run the persistent tests
+cabal_install "persistent"
