e2273e04ca6f1fc1672060b007784a47508ed347
Greg Weber <greg@gregweber.info>
2011-09-05 23:10:21 -0700

    fix up install script

--- install-all.sh
+++ install-all.sh
@@ -1,43 +1,52 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
-CABAL=cabal
+# allow a CABAL env var to override
+CABAL=${CABAL:-cabal}
+CONFIGURE_OPTS="--ghc-options=-Wall --ghc-options=-Werror"
 
 # install testing dependencies
-install_test_deps(){
-  $CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' 'file-location >= 0.4 && < 0.5'
-}
-
-install_test_deps ||
-  $CABAL update && install_test_deps
+$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' 'file-location >= 0.4 && < 0.5'
 
 # also pool and persistent
 PACKAGES="persistent-template persistent-sqlite persistent-postgresql persistent-mongoDB"
 
 cabal_install() {
-    if [ "$2" == "" ]
-    then
-      configure_opts="--enable-tests --ghc-options=-Wall --ghc-options=-Werror"
-      test="$CABAL test"
-    else
-      configure_opts=$2
-      test=""
-    fi
-    echo Installing $1
+  echo "Installing $1..."
+
+  (
     cd $1
-    ($CABAL configure $configure_opts ||
-      ($CABAL install --only-dependencies && $CABAL configure $configure_opts)
-    ) && $CABAL build && $test && $CABAL check && $CABAL haddock --executables && ./Setup.lhs install || exit 1
+
+    if [ "$3" = "--clean" ]; then
+      $CABAL clean
+    fi
+
+    if ! $CABAL configure $CONFIGURE_OPTS; then
+      $CABAL install --only-dependencies
+      $CABAL configure $CONFIGURE_OPTS
+    fi
+    $CABAL build
+
+    if [ "$2" = "test" ]; then
+      $CABAL configure --enable-tests
+      $CABAL build
+      $CABAL test
+    fi
+
+    $CABAL check
+    $CABAL haddock --executables
+    ./Setup.lhs install
     cd ..
+  )
 }
 
 # required for persistent
-cabal_install "pool"
+cabal_install "pool" "test" $@
 # don't run the persistent tests right now
-cabal_install "persistent" " "
+cabal_install "persistent" "" $@
 
 for package in $PACKAGES
 do
-  cabal_install $package
+  cabal_install $package "test"
 done
 
 # now run the persistent tests
