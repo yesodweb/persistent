3cfe4e41582d16ab5e31803ef139b1fa35026870
Michael Snoyman <michael@snoyman.com>
2011-12-05 09:23:59 +0200

    Fixed broken monad-control stuff

--- persistent/Database/Persist/GenericSql/Raw.hs
+++ persistent/Database/Persist/GenericSql/Raw.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Database.Persist.GenericSql.Raw
     ( withStmt
     , execute
@@ -22,9 +23,10 @@ import Control.Monad.Trans.Reader
 import qualified Data.Map as Map
 import Control.Applicative (Applicative)
 import Control.Monad.Trans.Class (MonadTrans (..))
+import Control.Monad.Base (MonadBase (liftBase))
 #if MIN_VERSION_monad_control(0, 3, 0)
-import Control.Monad.Trans.Control (MonadBaseControl)
-import Control.Monad.Base (MonadBase)
+import Control.Monad.Trans.Control (MonadBaseControl (..), ComposeSt, defaultLiftBaseWith, defaultRestoreM, MonadTransControl (..))
+import Control.Monad (liftM)
 #define MBCIO MonadBaseControl IO
 #else
 import Control.Monad.IO.Control (MonadControlIO)
@@ -40,17 +42,25 @@ newtype SqlPersist m a = SqlPersist { unSqlPersist :: ReaderT Connection m a }
 #endif
       )
 
-instance (Applicative m, Monad m) => MonadBase m (SqlPersist m) -- FIXME
+instance MonadBase b m => MonadBase b (SqlPersist m) where
+    liftBase = lift . liftBase
 
 #if MIN_VERSION_monad_control(0, 3, 0)
-instance (Applicative m, Monad m) => MonadBaseControl m (SqlPersist m) -- FIXME
+instance MonadBaseControl b m => MonadBaseControl b (SqlPersist m) where
+     newtype StM (SqlPersist m) a = StMSP {unStMSP :: ComposeSt SqlPersist m a}
+     liftBaseWith = defaultLiftBaseWith StMSP
+     restoreM     = defaultRestoreM   unStMSP
+instance MonadTransControl SqlPersist where
+    newtype StT SqlPersist a = StReader {unStReader :: a}
+    liftWith f = SqlPersist $ ReaderT $ \r -> f $ \t -> liftM StReader $ runReaderT (unSqlPersist t) r
+    restoreT = SqlPersist . ReaderT . const . liftM unStReader
 #endif
 
 withStmt :: (MonadIO m, MBCIO m) => Text -> [PersistValue]
          -> (RowPopper (SqlPersist m) -> SqlPersist m a) -> SqlPersist m a
 withStmt sql vals pop = do
     stmt <- getStmt sql
-    ret <- error "FIXME" -- I.withStmt stmt vals pop
+    ret <- I.withStmt stmt vals pop
     liftIO $ reset stmt
     return ret
 
