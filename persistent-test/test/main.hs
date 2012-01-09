d85b0b0dd0a473ea58e5668cd18c21732ecfe6e7
Michael Snoyman <michael@snoyman.com>
2012-01-09 08:55:30 +0200

    DataTypeTest

--- persistent-test/test/main.hs
+++ persistent-test/test/main.hs
@@ -1,8 +1,9 @@
 import PersistentTest
 import RenameTest
+import DataTypeTest
 import Test.Hspec.Monadic (hspecX)
 
 main :: IO ()
 main = do
   runConn setup
-  hspecX $ specs >> renameSpecs
+  hspecX $ specs >> renameSpecs >> dataTypeSpecs
