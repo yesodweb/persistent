c85f94387b6a4e8fcda2182e82101b11902ff153
Greg Weber <greg@gregweber.info>
2012-02-02 07:19:01 -0600

    add documentation for embedded syntax

--- persistent/Database/Persist/EntityDef.hs
+++ persistent/Database/Persist/EntityDef.hs
@@ -36,6 +36,29 @@ newtype DBName = DBName { unDBName :: Text }
 
 type Attr = Text
 
+-- | EmbedNone signifies a simple type (Text, Int64, etc) that can already be persisted.
+-- An embeddded type is one that you also define in your persistent schema.
+-- So we have a schema like:
+--
+-- @
+--   [persistUpperCase|
+--
+--   Embedded no-migrate
+--       name String
+--
+--   HasEmbed no-migrate
+--       name   String
+--       simple Embedded
+--       list   [Embedded]
+--       set    Set Embedded
+-- @
+--
+-- Note the use of the no-migrate flag to tell SQL not to generate a migration for the embedded entity.
+-- In our schema we demonstrate the different supported types:
+--
+--    * EmbedSimple: just embed a single entity
+--    * EmbedList:   embed a list of entities
+--    * EmbedSet:    embed a set of entities
 data FieldType = EmbedNone   { unFieldType :: Text }
                | EmbedSimple { unFieldType :: Text }
                | EmbedList   { unFieldType :: Text }
