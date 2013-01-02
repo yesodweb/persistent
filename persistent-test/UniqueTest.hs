66f965b997abad40c88da0bf07deddc16b0c2e95
Greg Weber <greg@gregweber.info>
2013-01-01 16:47:55 -0800

    remove uniqueness tests for MongoDB

--- persistent-test/UniqueTest.hs
+++ persistent-test/UniqueTest.hs
@@ -45,39 +45,43 @@ cleanDB :: (PersistQuery m, PersistEntityBackend TestNonNull ~ PersistMonadBacke
 cleanDB = do
   deleteWhere ([] :: [Filter TestNonNull])
   deleteWhere ([] :: [Filter TestNull])
+
 db :: Action IO () -> Assertion
 db = db' cleanDB
 #endif
 
 specs :: Spec
-specs = describe "uniqueness constraints" $ do
-  it "are respected for non-nullable Ints" $ do
-    let ins = insert . TestNonNull
-    (db $ void $ ins 1 >>        ins 2)
-    (db $ void $ ins 1 >>        ins 2 >> ins 1) `shouldThrow` anyException
-    (db $ void $ ins 1 >>= \k -> ins 2 >> delete k >> ins 1)
-  it "are respected for nullable Ints" $ do
-    let ins a b = insert $ TestNull a b
-        ctx = ins 1 Nothing  >> ins 1 Nothing >> ins 1 Nothing >>
-              ins 1 (Just 3) >> ins 1 (Just 4)
-    (db $ void   ctx)
-    (db $ void $ ctx >> ins 1 (Just 3)) `shouldThrow` anyException
-    (db $ void $ ctx >> ins 1 (Just 4)) `shouldThrow` anyException
-    (db $ void $ ctx >>= \k -> delete k >> ins 1 (Just 4))
-#ifndef WITH_MONGODB
-  it "work for Checkmark" $ do
-    let ins k v a = insert $ TestCheckmark k v a
-        ctx = ins "name" "John"    Inactive
-           >> ins "name" "Stewart" Inactive
-           >> ins "name" "Doroty"  Active
-           >> ins "color" "blue"   Inactive
-    (db $ void ctx)
-    (db $ void $ ctx >> ins "name" "Melissa" Active) `shouldThrow` anyException
-    (db $ void $ ctx >> ins "name" "Melissa" Inactive)
-    (db $ void $ ctx >>= flip update [TestCheckmarkActive =. Active])
-    (db $ void $ do
-        void ctx
-        updateWhere [TestCheckmarkKey   ==. "name"]
-                    [TestCheckmarkActive =. Inactive]
-        ins "name" "Melissa" Active)
+specs = describe "uniqueness constraints" $
+#ifdef WITH_MONGODB
+  return ()
+#else
+  do
+    it "are respected for non-nullable Ints" $ do
+      let ins = insert . TestNonNull
+      (db $ void $ ins 1 >>        ins 2)
+      (db $ void $ ins 1 >>        ins 2 >> ins 1) `shouldThrow` anyException
+      (db $ void $ ins 1 >>= \k -> ins 2 >> delete k >> ins 1)
+    it "are respected for nullable Ints" $ do
+      let ins a b = insert $ TestNull a b
+          ctx = ins 1 Nothing  >> ins 1 Nothing >> ins 1 Nothing >>
+                ins 1 (Just 3) >> ins 1 (Just 4)
+      (db $ void   ctx)
+      (db $ void $ ctx >> ins 1 (Just 3)) `shouldThrow` anyException
+      (db $ void $ ctx >> ins 1 (Just 4)) `shouldThrow` anyException
+      (db $ void $ ctx >>= \k -> delete k >> ins 1 (Just 4))
+    it "work for Checkmark" $ do
+      let ins k v a = insert $ TestCheckmark k v a
+          ctx = ins "name" "John"    Inactive
+             >> ins "name" "Stewart" Inactive
+             >> ins "name" "Doroty"  Active
+             >> ins "color" "blue"   Inactive
+      (db $ void ctx)
+      (db $ void $ ctx >> ins "name" "Melissa" Active) `shouldThrow` anyException
+      (db $ void $ ctx >> ins "name" "Melissa" Inactive)
+      (db $ void $ ctx >>= flip update [TestCheckmarkActive =. Active])
+      (db $ void $ do
+          void ctx
+          updateWhere [TestCheckmarkKey   ==. "name"]
+                      [TestCheckmarkActive =. Inactive]
+          ins "name" "Melissa" Active)
 #endif
