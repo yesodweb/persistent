7de082b7001f4e2c26f538b7bffafdb29c06e311
Michael Snoyman <michael@snoyman.com>
2013-03-24 11:25:00 +0200

    Added missing module

---
+++ persistent/Database/Persist/Types.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
+module Database.Persist.Types
+    ( module Database.Persist.Types.Base
+    , SomePersistField (..)
+    , Update (..)
+    , SelectOpt (..)
+    , BackendSpecificFilter
+    , Filter (..)
+    , Key
+    , Entity (..)
+    ) where
+
+import Database.Persist.Types.Base
+import Database.Persist.Class.PersistField
+import Database.Persist.Class.PersistEntity
