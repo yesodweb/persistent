43bda27d8cc40b7e6d37bcda7a73d35d396f5462
Greg Weber <greg@gregweber.info>
2013-06-29 19:07:25 -0700

    replaceUnique function, re-organize non-typeclass functions
    replaceUnique needed functions (getJust) that were living in the base persistent
    module. This caused circular imports.

--- persistent/Database/Persist/Class/PersistUnique.hs
+++ persistent/Database/Persist/Class/PersistUnique.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 module Database.Persist.Class.PersistUnique
     ( PersistUnique (..)
+    , getByValue
+    , insertBy
+    , replaceUnique
     ) where
 
 import qualified Prelude
@@ -11,6 +14,7 @@ import Control.Monad (liftM)
 import Control.Monad.Trans.Error (Error (..))
 import Control.Monad.Trans.Class (lift)
 import Data.Monoid (Monoid)
+import Data.List ((\\))
 
 import Data.Conduit.Internal (Pipe)
 import Control.Monad.Logger (LoggingT)
@@ -50,6 +54,57 @@ class PersistStore m => PersistUnique m where
     insertUnique datum = do
         isUnique <- checkUnique datum
         if isUnique then Just `liftM` insert datum else return Nothing
+
+-- | Insert a value, checking for conflicts with any unique constraints.  If a
+-- duplicate exists in the database, it is returned as 'Left'. Otherwise, the
+-- new 'Key is returned as 'Right'.
+insertBy :: (PersistEntity v, PersistStore m, PersistUnique m, PersistMonadBackend m ~ PersistEntityBackend v)
+          => v -> m (Either (Entity v) (Key v))
+insertBy val =
+    go $ persistUniqueKeys val
+  where
+    go [] = Right `liftM` insert val
+    go (x:xs) = do
+        y <- getBy x
+        case y of
+            Nothing -> go xs
+            Just z -> return $ Left z
+
+-- | A modification of 'getBy', which takes the 'PersistEntity' itself instead
+-- of a 'Unique' value. Returns a value matching /one/ of the unique keys. This
+-- function makes the most sense on entities with a single 'Unique'
+-- constructor.
+getByValue :: (PersistEntity v, PersistUnique m, PersistEntityBackend v ~ PersistMonadBackend m)
+           => v -> m (Maybe (Entity v))
+getByValue val =
+    go $ persistUniqueKeys val
+  where
+    go [] = return Nothing
+    go (x:xs) = do
+        y <- getBy x
+        case y of
+            Nothing -> go xs
+            Just z -> return $ Just z
+
+-- | attempt to replace the record of the given key with the given new record
+-- First query the unique fields to make sure the replacement maintains uniqueness constraints
+-- Return true if the replacement was made, false if it was not because it violates uniqueness.
+replaceUnique :: (Eq record, Eq (Unique record), PersistEntityBackend record ~ PersistMonadBackend m, PersistEntity record, PersistStore m, PersistUnique m)
+               => Key record -> record -> m Bool
+replaceUnique key datumNew = getJust key >>= replaceOriginal
+  where
+    uniqueKeysNew = persistUniqueKeys datumNew
+    replaceOriginal original = replaceByKeys changedKeys
+      where
+        changedKeys = uniqueKeysOriginal \\ uniqueKeysNew
+        uniqueKeysOriginal = persistUniqueKeys original
+
+    replaceByKeys [] = replace key datumNew >> return True
+    replaceByKeys (key:keys) = do
+      conflict <- getBy key
+      case conflict of
+           Nothing -> replaceByKeys keys
+           (Just _) -> return False
 
 #define DEF(T) { getBy = lift . getBy; deleteBy = lift . deleteBy; insertUnique = lift . insertUnique }
 #define GO(T) instance (PersistUnique m) => PersistUnique (T m) where DEF(T)
