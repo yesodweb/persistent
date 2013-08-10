9de373b6488922fb41c96ec1a41c2fe3eb633a65
Greg Weber <greg@gregweber.info>
2013-08-10 16:11:46 -0700

    support embedding of an Entity
    This is really for MongoDB.
    Also support the ObjectId type via Objectid

---
+++ persistent-test/EntityEmbedTest.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+module EntityEmbedTest where
+import Init
+
+-- because we are using a type alias we need to declare in a separate module
+-- this is used in EmbedTest
+mkPersist persistSettings [persistUpperCase|
+  ARecord
+    name Text
+    deriving Show Eq Read Ord
+|]
+
+type AnEntity = Entity ARecord
