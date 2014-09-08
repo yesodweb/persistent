4c158d15779dcdde0051c4339e28f0a2480669be
Greg Weber <greg@gregweber.info>
2014-09-08 08:26:35 -0700

    don't assume an integer key

--- persistent/Database/Persist/Sql/Orphan/PersistStore.hs
+++ persistent/Database/Persist/Sql/Orphan/PersistStore.hs
@@ -33,169 +33,169 @@ withRawQuery sql vals sink = do
     liftIO $ with srcRes (C.$$ sink)
 
 instance PersistStore Connection where
-    newtype BackendKey Connection = SqlBackendKey { unSqlBackendKey :: Int64 }
-        deriving (Show, Read, Eq, Ord, Num, Integral, PersistField, PersistFieldSql, PathPiece, Real, Enum, Bounded, A.ToJSON, A.FromJSON)
+  newtype BackendKey Connection = SqlBackendKey { unSqlBackendKey :: Int64 }
+      deriving (Show, Read, Eq, Ord, Num, Integral, PersistField, PersistFieldSql, PathPiece, Real, Enum, Bounded, A.ToJSON, A.FromJSON)
 
-    update _ [] = return ()
-    update k upds = do
-        conn <- ask
-        let go'' n Assign = n <> "=?"
-            go'' n Add = T.concat [n, "=", n, "+?"]
-            go'' n Subtract = T.concat [n, "=", n, "-?"]
-            go'' n Multiply = T.concat [n, "=", n, "*?"]
-            go'' n Divide = T.concat [n, "=", n, "/?"]
-        let go' (x, pu) = go'' (connEscapeName conn x) pu
-        let wher = case entityPrimary t of
-                Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
-                Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
-        let sql = T.concat
-                [ "UPDATE "
-                , connEscapeName conn $ entityDB t
-                , " SET "
-                , T.intercalate "," $ map (go' . go) upds
-                , " WHERE "
-                , wher
-                ]
-        rawExecute sql $
-            map updatePersistValue upds `mappend` keyToValues k
-      where
-        t = entityDef $ dummyFromKey k
-        go x = (fieldDB $ updateFieldDef x, updateUpdate x)
+  update _ [] = return ()
+  update k upds = do
+      conn <- ask
+      let go'' n Assign = n <> "=?"
+          go'' n Add = T.concat [n, "=", n, "+?"]
+          go'' n Subtract = T.concat [n, "=", n, "-?"]
+          go'' n Multiply = T.concat [n, "=", n, "*?"]
+          go'' n Divide = T.concat [n, "=", n, "/?"]
+      let go' (x, pu) = go'' (connEscapeName conn x) pu
+      let wher = case entityPrimary t of
+              Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
+              Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
+      let sql = T.concat
+              [ "UPDATE "
+              , connEscapeName conn $ entityDB t
+              , " SET "
+              , T.intercalate "," $ map (go' . go) upds
+              , " WHERE "
+              , wher
+              ]
+      rawExecute sql $
+          map updatePersistValue upds `mappend` keyToValues k
+    where
+      t = entityDef $ dummyFromKey k
+      go x = (fieldDB $ updateFieldDef x, updateUpdate x)
 
-    insert val = do
-        conn <- ask
-        let esql = connInsertSql conn t vals
-        key <-
-            case esql of
-                ISRSingle sql -> withRawQuery sql vals $ do
-                    x <- CL.head
-                    case x of
-                        Just [PersistInt64 i] -> case keyFromValues [PersistInt64 i] of
-                            Left err -> error $ "SQL insert: keyFromValues: PersistInt64 " `mappend` show i `mappend` " " `mappend` unpack err
+  insert val = do
+      conn <- ask
+      let esql = connInsertSql conn t vals
+      key <- case esql of
+        ISRSingle sql -> withRawQuery sql vals $ do
+            x <- CL.head
+            case x of
+                Just [PersistInt64 i] -> case keyFromValues [PersistInt64 i] of
+                    Left err -> error $ "SQL insert: keyFromValues: PersistInt64 " `mappend` show i `mappend` " " `mappend` unpack err
+                    Right k -> return k
+                Nothing -> error $ "SQL insert did not return a result giving the generated ID"
+                Just vals' -> case keyFromValues vals' of
+                    Left _ -> error $ "Invalid result from a SQL insert, got: " ++ show vals'
+                    Right k -> return k
+
+        ISRInsertGet sql1 sql2 -> do
+            rawExecute sql1 vals
+            withRawQuery sql2 [] $ do
+                mm <- CL.head
+                let ks = case mm of
+                        Just [PersistInt64 i]  -> [PersistInt64 i]
+                        Just [PersistDouble i] -> [PersistInt64 $ truncate i] -- oracle need this!
+                        Just [PersistByteString i] -> case readInteger i of -- mssql
+                                                        Just (ret,"") -> [PersistInt64 $ fromIntegral ret]
+                                                        xs -> error $ "invalid number i["++show i++"] xs[" ++ show xs ++ "]"
+                        Just xs -> xs -- error $ "invalid sql2 return xs["++show xs++"] sql2["++show sql2++"] sql1["++show sql1++"]"
+                        Nothing -> error $ "invalid sql2 returned nothing sql2["++show sql2++"] sql1["++show sql1++"]"
+                case keyFromValues ks of
+                    Right k -> return k
+                    Left err -> error $ "ISRInsertGet: keyFromValues failed: " `mappend` unpack err
+
+        ISRManyKeys sql fs -> do
+            rawExecute sql vals 
+            case entityPrimary t of
+               Nothing -> error $ "ISRManyKeys is used when Primary is defined " ++ show sql
+               Just pdef -> 
+                    let pks = map fieldHaskell $ compositeFields pdef
+                        keyvals = map snd $ filter (\(a, _) -> let ret=isJust (find (== a) pks) in ret) $ zip (map fieldHaskell $ entityFields t) fs
+                    in  case keyFromValues keyvals of
                             Right k -> return k
-                        Nothing -> error $ "SQL insert did not return a result giving the generated ID"
-                        Just vals' -> case keyFromValues vals' of
-                            Left _ -> error $ "Invalid result from a SQL insert, got: " ++ show vals'
-                            Right k -> return k
+                            Left e  -> error $ "ISRManyKeys: unexpected keyvals result: " `mappend` unpack e
 
-                ISRInsertGet sql1 sql2 -> do
-                    rawExecute sql1 vals
-                    withRawQuery sql2 [] $ do
-                        mm <- CL.head
-                        i <- case mm of
-                            Just [PersistInt64 i] -> return $ i
-                            Just [PersistDouble i] ->return $ truncate i -- oracle need this!
-                            Just [PersistByteString i] -> case readInteger i of -- mssql
-                                                            Just (ret,"") -> return $ fromIntegral ret
-                                                            xs -> error $ "invalid number i["++show i++"] xs[" ++ show xs ++ "]"
-                            Just xs -> error $ "invalid sql2 return xs["++show xs++"] sql2["++show sql2++"] sql1["++show sql1++"]"
-                            Nothing -> error $ "invalid sql2 returned nothing sql2["++show sql2++"] sql1["++show sql1++"]"
-                        case keyFromValues [PersistInt64 i] of
-                            Right k -> return k
-                            Left err -> error $ "ISRInsertGet: keyFromValues failed: " `mappend` unpack err
-                ISRManyKeys sql fs -> do
-                    rawExecute sql vals 
-                    case entityPrimary t of
-                       Nothing -> error $ "ISRManyKeys is used when Primary is defined " ++ show sql
-                       Just pdef -> 
-                            let pks = map fieldHaskell $ compositeFields pdef
-                                keyvals = map snd $ filter (\(a, _) -> let ret=isJust (find (== a) pks) in ret) $ zip (map fieldHaskell $ entityFields t) fs
-                            in  case keyFromValues keyvals of
-                                    Right k -> return k
-                                    Left e  -> error $ "ISRManyKeys: unexpected keyvals result: " `mappend` unpack e
+      return key
+    where
+      t = entityDef $ Just val
+      vals = map toPersistValue $ toPersistFields val
 
-        return key
-      where
-        t = entityDef $ Just val
-        vals = map toPersistValue $ toPersistFields val
+  insertMany_ vals = do
+      conn <- ask
+      let sql = T.concat
+              [ "INSERT INTO "
+              , connEscapeName conn (entityDB t)
+              , "("
+              , T.intercalate "," $ map (connEscapeName conn . fieldDB) $ entityFields t
+              , ") VALUES ("
+              , T.intercalate "),(" $ replicate (length valss) $ T.intercalate "," $ map (const "?") (entityFields t)
+              , ")"
+              ]
 
-    insertMany_ vals = do
-        conn <- ask
-        let sql = T.concat
-                [ "INSERT INTO "
-                , connEscapeName conn (entityDB t)
-                , "("
-                , T.intercalate "," $ map (connEscapeName conn . fieldDB) $ entityFields t
-                , ") VALUES ("
-                , T.intercalate "),(" $ replicate (length valss) $ T.intercalate "," $ map (const "?") (entityFields t)
-                , ")"
-                ]
+      -- SQLite support is only in later versions
+      if connRDBMS conn == "sqlite"
+          then mapM_ insert vals
+          else rawExecute sql (concat valss)
+    where
+      t = entityDef vals
+      valss = map (map toPersistValue . toPersistFields) vals
 
-        -- SQLite support is only in later versions
-        if connRDBMS conn == "sqlite"
-            then mapM_ insert vals
-            else rawExecute sql (concat valss)
-      where
-        t = entityDef vals
-        valss = map (map toPersistValue . toPersistFields) vals
+  replace k val = do
+      conn <- ask
+      let t = entityDef $ Just val
+      let sql = T.concat
+              [ "UPDATE "
+              , connEscapeName conn (entityDB t)
+              , " SET "
+              , T.intercalate "," (map (go conn . fieldDB) $ entityFields t)
+              , " WHERE "
+              , connEscapeName conn $ sqlIdName t
+              , "=?"
+              ]
+          vals = map toPersistValue (toPersistFields val) `mappend` keyToValues k
+      rawExecute sql vals
+    where
+      go conn x = connEscapeName conn x `T.append` "=?"
 
-    replace k val = do
-        conn <- ask
-        let t = entityDef $ Just val
-        let sql = T.concat
-                [ "UPDATE "
-                , connEscapeName conn (entityDB t)
-                , " SET "
-                , T.intercalate "," (map (go conn . fieldDB) $ entityFields t)
-                , " WHERE "
-                , connEscapeName conn $ sqlIdName t
-                , "=?"
-                ]
-            vals = map toPersistValue (toPersistFields val) `mappend` keyToValues k
-        rawExecute sql vals
-      where
-        go conn x = connEscapeName conn x `T.append` "=?"
+  insertKey = insrepHelper "INSERT"
 
-    insertKey = insrepHelper "INSERT"
+  repsert key value = do
+      mExisting <- get key
+      case mExisting of
+        Nothing -> insertKey key value
+        Just _ -> replace key value
 
-    repsert key value = do
-        mExisting <- get key
-        case mExisting of
-          Nothing -> insertKey key value
-          Just _ -> replace key value
+  get k = do
+      conn <- ask
+      let t = entityDef $ dummyFromKey k
+      let cols = T.intercalate ","
+               $ map (connEscapeName conn . fieldDB) $ entityFields t
+          noColumns :: Bool
+          noColumns = null $ entityFields t
+      let wher = case entityPrimary t of
+                   Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
+                   Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
+      let sql = T.concat
+              [ "SELECT "
+              , if noColumns then "*" else cols
+              , " FROM "
+              , connEscapeName conn $ entityDB t
+              , " WHERE "
+              , wher
+              ]
+      withRawQuery sql (keyToValues k) $ do
+          res <- CL.head
+          case res of
+              Nothing -> return Nothing
+              Just vals ->
+                  case fromPersistValues $ if noColumns then [] else vals of
+                      Left e -> error $ "get " ++ show k ++ ": " ++ unpack e
+                      Right v -> return $ Just v
 
-    get k = do
-        conn <- ask
-        let t = entityDef $ dummyFromKey k
-        let cols = T.intercalate ","
-                 $ map (connEscapeName conn . fieldDB) $ entityFields t
-            noColumns :: Bool
-            noColumns = null $ entityFields t
-        let wher = case entityPrimary t of
-                     Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
-                     Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
-        let sql = T.concat
-                [ "SELECT "
-                , if noColumns then "*" else cols
-                , " FROM "
-                , connEscapeName conn $ entityDB t
-                , " WHERE "
-                , wher
-                ]
-        withRawQuery sql (keyToValues k) $ do
-            res <- CL.head
-            case res of
-                Nothing -> return Nothing
-                Just vals ->
-                    case fromPersistValues $ if noColumns then [] else vals of
-                        Left e -> error $ "get " ++ show k ++ ": " ++ unpack e
-                        Right v -> return $ Just v
-
-    delete k = do
-        conn <- ask
-        rawExecute (sql conn) (keyToValues k)
-      where
-        t = entityDef $ dummyFromKey k
-        wher conn = 
-              case entityPrimary t of
-                Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
-                Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
-        sql conn = T.concat
-            [ "DELETE FROM "
-            , connEscapeName conn $ entityDB t
-            , " WHERE "
-            , wher conn
-            ]
+  delete k = do
+      conn <- ask
+      rawExecute (sql conn) (keyToValues k)
+    where
+      t = entityDef $ dummyFromKey k
+      wher conn = 
+            case entityPrimary t of
+              Just pdef -> T.intercalate " AND " $ map (\fld -> connEscapeName conn (fieldDB fld) <> "=? ") $ compositeFields pdef
+              Nothing   -> connEscapeName conn (sqlIdName t) <> "=?"
+      sql conn = T.concat
+          [ "DELETE FROM "
+          , connEscapeName conn $ entityDB t
+          , " WHERE "
+          , wher conn
+          ]
 
 dummyFromKey :: Key v -> Maybe v
 dummyFromKey _ = Nothing
