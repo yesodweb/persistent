fab65e9bd790318f3edfc1966f0a829f5c19cd1b
Matt Parsons <parsonsmatt@gmail.com>
2019-04-02 14:03:14 -0600

    Add benchmark for nullable parsing (#870)
    * Add benchmark for nullable parsing
    
    * Switch to nfIO
    
    * Benchmark the mkPersist
    
    * Add from file benchmark
    
    * Comment out benchmarks because compiling them takes 50s

--- persistent-template/bench/Models.hs
+++ persistent-template/bench/Models.hs
@@ -3,22 +3,43 @@ module Models where
 import Data.Monoid
 import Data.List
 import Data.FileEmbed
+import Language.Haskell.TH
+import qualified Data.Text as Text
+
+import Database.Persist.Quasi
+import Database.Persist.TH
+import Database.Persist.Sql
+
+mkPersist' :: [EntityDef] -> IO [Dec]
+mkPersist' = runQ . mkPersist sqlSettings
+
+parseReferences' :: String -> IO Exp
+parseReferences' = runQ . parseReferencesQ
+
+parseReferencesQ :: String -> Q Exp
+parseReferencesQ = parseReferences lowerCaseSettings . Text.pack
 
 -- | # of models, # of fields
 mkModels :: Int -> Int -> String
-mkModels i f = unlines . fmap unlines . take i . map mkModel . zip [0..] . cycle $
-    [ "Model"
-    , "Foobar"
-    , "User"
-    , "King"
-    , "Queen"
-    , "Dog"
-    , "Cat"
-    ]
+mkModels = mkModelsWithFieldModifier id
+
+mkNullableModels :: Int -> Int -> String
+mkNullableModels = mkModelsWithFieldModifier maybeFields
+
+mkModelsWithFieldModifier :: (String -> String) -> Int -> Int -> String
+mkModelsWithFieldModifier k i f =
+    unlines . fmap unlines . take i . map mkModel . zip [0..] . cycle $
+        [ "Model"
+        , "Foobar"
+        , "User"
+        , "King"
+        , "Queen"
+        , "Dog"
+        , "Cat"
+        ]
   where
     mkModel (i, m) =
-        (m <> show i) : indent 4 (mkFields f)
-
+        (m <> show i) : indent 4 (map k (mkFields f))
 
 indent :: Int -> [String] -> [String]
 indent i = map (replicate i ' ' ++)
@@ -28,9 +49,11 @@ mkFields i = take i $ map mkField $ zip [0..] $ cycle
     [ "Bool"
     , "Int"
     , "String"
-    , "Char"
     , "Double"
+    , "Text"
     ]
   where
     mkField (i, typ) = "field" <> show i <> "\t\t" <> typ
 
+maybeFields :: String -> String
+maybeFields = (++ " Maybe")
