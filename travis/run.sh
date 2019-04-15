a8c16afd0e342781176272d4d17352ca0f9e4ca5
parsonsmatt <parsonsmatt@gmail.com>
2019-04-14 20:54:00 -0600

    Tidying up a few things
    * Add support for building with cabal new-*
    * Build benchmarks in CI (fixes #890)
    * Add docs for testing persistent-postgresql
    * Add a few badges
    * Remove confusing files
    * Remove references to running persistent-test

--- travis/run.sh
+++ travis/run.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 psql -c 'create database persistent;' -U postgres
 mysql -e 'create database persistent;'
-exec stack $ARGS --no-terminal test
+exec stack $ARGS --no-terminal test --bench --no-run-benchmarks
