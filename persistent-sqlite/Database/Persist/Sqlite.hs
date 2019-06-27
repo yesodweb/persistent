2cace2cbe91dd8b439c4aea54eb411d7e873f662
kderme <k.dermenz@gmail.com>
2019-06-28 01:00:36 +0300

    Fix Unique Constraints in SQlite

--- persistent-sqlite/Database/Persist/Sqlite.hs
+++ persistent-sqlite/Database/Persist/Sqlite.hs
@@ -522,6 +522,7 @@ mkCreateTable isTemp entity (cols, uniqs) =
         , "("
         , T.intercalate "," $ map (escape . fieldDB) $ compositeFields pdef
         , ")"
+        , T.concat $ map sqlUnique uniqs
         , ")"
         ]
     Nothing -> T.concat
