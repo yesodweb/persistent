3d4504bf7c84e6701c8fb8c1c3ea305c2ac198b2
Rob Bassi <rob.bassi.89@gmail.com>
2019-10-31 11:16:31 -0400

    Refactored tests

--- persistent-postgresql/test/CustomConstraintTest.hs
+++ persistent-postgresql/test/CustomConstraintTest.hs
@@ -11,7 +11,6 @@
 module CustomConstraintTest where
 
 import PgInit
-import Data.List (find)
 import qualified Data.Text as T
 
 share [mkPersist sqlSettings, mkMigrate "customConstraintMigrate"] [persistLowerCase|
@@ -24,23 +23,29 @@ CustomConstraint2
     deriving Show
 |]
 
-alterQuery :: Text
-alterQuery = "ALTER TABLE \"custom_constraint2\" ADD CONSTRAINT \"custom_constraint\" FOREIGN KEY(\"cc_id\") REFERENCES \"custom_constraint1\"(\"id\")"
-
 specs :: (MonadIO m, MonadFail m) => RunDb SqlBackend m -> Spec
 specs runDb = do
   describe "custom constraint used in migration" $ do
-    it "alter table uses custom constraint name" $ runDb $ do
-      ms <- getMigration customConstraintMigrate
-      let alter = find (== alterQuery) ms
-      liftIO $ alter @?= Just alterQuery
     it "custom constraint is actually created" $ runDb $ do
       runMigration customConstraintMigrate
       runMigration customConstraintMigrate -- run a second time to ensure the constraint isn't dropped
-      let query = T.concat ["SELECT COUNT(*) "
-                           ,"FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS "
-                           ,"WHERE TABLE_NAME='custom_constraint2' "
-                           ,"AND CONSTRAINT_NAME='custom_constraint'"]
-      [Single exists] <- rawSql query []
+      let query = T.concat ["SELECT DISTINCT COUNT(*) "
+                           ,"FROM information_schema.constraint_column_usage ccu, "
+                           ,"information_schema.key_column_usage kcu, "
+                           ,"information_schema.table_constraints tc "
+                           ,"WHERE tc.constraint_type='FOREIGN KEY' "
+                           ,"AND kcu.constraint_name=tc.constraint_name "
+                           ,"AND ccu.constraint_name=kcu.constraint_name "
+                           ,"AND kcu.ordinal_position=1 "
+                           ,"AND ccu.table_name=? "
+                           ,"AND ccu.column_name=? "
+                           ,"AND kcu.table_name=? "
+                           ,"AND kcu.column_name=? "
+                           ,"AND tc.constraint_name=?"]
+      [Single exists] <- rawSql query [PersistText "custom_constraint1"
+                                      ,PersistText "id"
+                                      ,PersistText "custom_constraint2"
+                                      ,PersistText "cc_id"
+                                      ,PersistText "custom_constraint"]
       liftIO $ 1 @?= (exists :: Int)
 
