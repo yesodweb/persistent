756601397a5e2653f8da2fd27cbe3f9d7f3984d8
Maximilian Tagher <feedback.tagher@gmail.com>
2020-06-07 17:07:12 -0400

    ..

---
+++ persistent-template/compile-time-testing/add-timings.rb
@@ -0,0 +1,24 @@
+#!ruby -w
+
+directory = ARGV[0]
+puts "Looking for data in #{directory}"
+
+Dir.chdir(directory)
+filenames = Dir.glob('*.dump-timings')
+
+totals = []
+
+filenames.each do |name|
+  text = File.read(name)
+  lines = text.split("\n")
+  total = 0
+  lines.each do |line|
+    start, time = line.split("time=")
+    total += time.to_f
+  end
+
+  totals << total
+end
+
+mean = totals.inject(0.0) { |sum, el| sum + el } / totals.size
+puts "Mean is #{mean}"
