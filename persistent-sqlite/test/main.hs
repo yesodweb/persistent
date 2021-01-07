243a605e7354625fe8c59cd3cf55f79e964962e7
Merijn Verstraaten <merijn@inconsistent.nl>
2021-01-07 15:50:40 +0100

    Add test to detect broken migrations from #1184.

--- persistent-sqlite/test/main.hs
+++ persistent-sqlite/test/main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -46,16 +47,21 @@ import qualified UniqueTest
 import qualified UpsertTest
 import qualified LongIdentifierTest
 
-import Control.Exception (handle, IOException)
+import Control.Exception (handle, IOException, throwIO)
 import Control.Monad.Catch (catch)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (MonadReader)
+import Control.Monad.Trans.Resource (MonadResource)
 import qualified Data.ByteString as BS
+import Data.Conduit ((.|), runConduit)
+import qualified Data.Conduit.List as CL
 import Data.Fixed
 import Data.IntMap (IntMap)
 import qualified Data.Text as T
 import Data.Time
 import Filesystem (removeFile)
 import Filesystem.Path.CurrentOS (fromText)
+import qualified Lens.Micro as Lens
 import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
 import System.IO (hClose)
 import System.IO.Temp (withSystemTempFile)
@@ -88,19 +94,56 @@ DataTypeTable no-json
 |]
 
 share [mkPersist sqlSettings, mkMigrate "compositeSetup"] [persistLowerCase|
-Simple
+SimpleComposite
     int Int
     text Text
     Primary text int
     deriving Show Eq
+
+SimpleCompositeReference
+    int Int
+    text Text
+    label Text
+    Foreign SimpleComposite fk_simple_composite text int
+    deriving Show Eq
 |]
 
 share [mkPersist sqlSettings, mkMigrate "compositeMigrateTest"] [persistLowerCase|
-Simple2 sql=simple
+SimpleComposite2 sql=simple_composite
     int Int
     text Text
-    bool Bool
+    new Int default=0
     Primary text int
+    deriving Show Eq
+
+SimpleCompositeReference2 sql=simple_composite_reference
+    int Int
+    text Text
+    label Text
+    Foreign SimpleComposite2 fk_simple_composite text int
+    deriving Show Eq
+|]
+
+share [mkPersist sqlSettings, mkMigrate "idSetup"] [persistLowerCase|
+Simple
+    text Text
+    deriving Show Eq
+
+SimpleReference
+    simpleCompositeId SimpleId
+    text Text
+    deriving Show Eq
+|]
+
+share [mkPersist sqlSettings, mkMigrate "idMigrateTest"] [persistLowerCase|
+Simple2 sql=simple
+    text Text
+    int Int default=0
+    deriving Show Eq
+
+SimpleReference2 sql=simple_reference
+    simpleCompositeId Simple2Id
+    text Text
     deriving Show Eq
 |]
 
@@ -247,6 +290,38 @@ main = do
         void $ runMigrationSilent compositeMigrateTest
         pure ()
 
+    it "test migrating sparse primary keys (issue #1184)" $ asIO $ withSystemTempFile "test564.sqlite3"$ \fp h -> do
+        hClose h
+        let connInfo = Lens.set fkEnabled False $ mkSqliteConnectionInfo (T.pack fp)
+        runSqliteInfo connInfo $ do
+            void $ runMigrationSilent idSetup
+            forM_ (map toSqlKey [1,3]) $ \key -> do
+                insertKey key (Simple "foo")
+                insert (SimpleReference key "test")
+
+            validateForeignKeys
+
+        runSqliteInfo connInfo $ do
+            void $ runMigrationSilent idMigrateTest
+            validateForeignKeys
+
+    it "test migrating sparse composite primary keys (issue #1184)" $ asIO $ withSystemTempFile "test564.sqlite3"$ \fp h -> do
+        hClose h
+        let connInfo = Lens.set fkEnabled False $ mkSqliteConnectionInfo (T.pack fp)
+
+        runSqliteInfo connInfo $ do
+            void $ runMigrationSilent compositeSetup
+            forM_ [(1,"foo"),(3,"bar")] $ \(intKey, strKey) -> do
+                let key = SimpleCompositeKey strKey intKey
+                insertKey key (SimpleComposite intKey strKey)
+                insert (SimpleCompositeReference intKey strKey "test")
+
+            validateForeignKeys
+
+        runSqliteInfo connInfo $ do
+            void $ runMigrationSilent compositeMigrateTest
+            validateForeignKeys
+
     it "afterException" $ asIO $ runSqliteInfo (mkSqliteConnectionInfo ":memory:") $ do
         void $ runMigrationSilent testMigrate
         let catcher :: forall m. Monad m => SomeException -> m ()
@@ -255,3 +330,11 @@ main = do
         insert_ (Person "A" 1 Nothing) `catch` catcher
         insert_ $ Person "B" 0 Nothing
         return ()
+
+validateForeignKeys
+    :: (MonadResource m, MonadReader env m, BackendCompatible SqlBackend env)
+    => m ()
+validateForeignKeys = do
+    violations <- map (T.pack . show) <$> runConduit (checkForeignKeys .| CL.consume)
+    unless (null violations) . liftIO . throwIO $
+        PersistForeignConstraintUnmet (T.unlines violations)
