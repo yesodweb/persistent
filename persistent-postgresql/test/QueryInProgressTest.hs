4b1d1dea493d9bbe45f5cb0271077f929d8b3a87
codygman <cody@codygman.dev>
2021-03-15 11:59:54 -0500

    update test

--- persistent-postgresql/test/QueryInProgressTest.hs
+++ persistent-postgresql/test/QueryInProgressTest.hs
@@ -16,18 +16,20 @@
 
 module QueryInProgressTest where
 
-import PgInit
-import qualified Control.Monad as Monad
-import Control.Monad.IO.Class
-import qualified Control.Concurrent as Concurrent
-import qualified Control.Exception as Exception
-import qualified Database.Persist as Persist
-import qualified Database.Persist.Sql as Persist
-import qualified Database.Persist.Postgresql as Persist
-import qualified Control.Monad.Logger as Logger
-import qualified Data.ByteString as BS
-import qualified Data.Pool as Pool
-import Data.Time
+import qualified Control.Concurrent            as Concurrent
+import qualified Control.Concurrent.Async      as Concurrent
+import qualified Control.Exception             as Exception
+import qualified Control.Monad                 as Monad
+import           Control.Monad.IO.Class
+import qualified Control.Monad.Logger          as Logger
+import qualified Data.ByteString               as BS
+import           Data.Either
+import qualified Data.Pool                     as Pool
+import           Data.Time
+import qualified Database.Persist              as Persist
+import qualified Database.Persist.Postgresql   as Persist
+import qualified Database.Persist.Sql          as Persist
+import           PgInit
 -- import Database.PostgreSQL.LibPQ (transactionStatus)
 
 share [mkPersist sqlSettings, mkMigrate "migration"] [persistUpperCase|
@@ -40,47 +42,68 @@ share [mkPersist sqlSettings, mkMigrate "migration"] [persistUpperCase|
 |]
 
 createTableFoo :: Pool.Pool Persist.SqlBackend -> IO ()
-createTableFoo pool = (flip Persist.runSqlPersistMPool) pool $ do
-  Persist.rawExecute "CREATE table if not exists foobar(id int);" []
+createTableFoo pool = flip Persist.runSqlPersistMPool pool
+    $ Persist.rawExecute "CREATE table if not exists foobar(id int);" []
+
+testInterruptedConnection = do
+    pool <- Logger.runNoLoggingT $ Persist.createPostgresqlPool
+        "postgresql://postgres:secret@localhost/postgres"
+        1
+
+    Monad.void $ createTableFoo pool
+
+    simulateFailedLongRunningPostgresCall pool
+
+    result :: Either SomeException [Maybe (Single String)] <-
+        Exception.try . flip Persist.runSqlPool pool $ do
+
+            Persist.rawSql @(Maybe (Persist.Single String))
+                "select pg_sleep(5)"
+                []
+
+    pure result
 
 
 specsWith :: (MonadIO m, MonadFail m) => RunDb SqlBackend m -> Spec
-specsWith runDb = describe "QueryInProgress" $ do
-    fit ("queries are cleaned up correctly") $ do
+specsWith _ =
+    describe "QueryInProgress" $ fit "queries are cleaned up correctly" $ do
+        results <- Concurrent.replicateConcurrently
+            10
+            testInterruptedConnection
+        results `shouldNotSatisfy` any isLeft
 
-      pool <- Logger.runNoLoggingT $ Persist.createPostgresqlPool "postgresql://postgres:secret@localhost/postgres" 1
 
-      Monad.void $ createTableFoo pool
 
-      getCurrentTime >>= \now ->
-        simulateFailedLongRunningPostgresCall pool 
-
-      putStrLn "normal query"
-      putStrLn "first query"
-      void . ((flip Persist.runSqlPersistMPool) pool) $ do
-        Persist.rawSql @(Maybe (Persist.Single String)) "select pg_sleep(5)" []
-      putStrLn "normal query should have close/masking state message above"
-
-      result :: Either Exception.SomeException [Persist.Single String] <-
-        Exception.try . ((flip Persist.runSqlPersistMPool) pool) $ do
-        Persist.rawSql @(Persist.Single String) "select pg_sleep(5)" []
-
-      let exceptionStr :: String = either Exception.displayException (const "no exception") result
-      liftIO $ assertNotEqual "Did the libpq error happen?" "libpq: failed (another command is already in progress\n)" exceptionStr  
-      -- void $ rawSql @(Maybe (Single String)) "select pg_sleep(1)" []
-      
 
 
 simulateFailedLongRunningPostgresCall :: Pool.Pool Persist.SqlBackend -> IO ()
 simulateFailedLongRunningPostgresCall pool = do
-  threadId <- Concurrent.forkIO
-    $ (do
-        let numThings :: Int = 100000000
-        putStrLn $ "start inserting " <> show numThings <> " things"
-        Monad.forM_ [1 .. numThings] $ \_ -> do
-          (flip Persist.runSqlPersistMPool) pool $
-            Persist.rawExecute "insert into foobar values(1);" []
-      )
-  Concurrent.threadDelay 5000000
-  Monad.void $ Concurrent.killThread threadId
-  putStrLn "killed thread"
+    threadId <- Concurrent.forkIO
+        (do
+
+            putStrLn
+                "verify pool can't be borrowed and we've set things up correctly"
+
+            -- maybe
+            --   (putStrLn "pool exhausted")
+            --   (const $ putStrLn "pool could be borrowed")
+            --   =<< Pool.tryTakeResource pool
+
+
+            let numThings :: Int = 100000000
+            putStrLn $ "start inserting " <> show numThings <> " things"
+
+            Monad.forM_ [1 .. numThings] $ \i -> do
+                Logger.runStdoutLoggingT
+                    $ flip Persist.runSqlPool pool
+                    $
+
+                  -- this is basically calling Acquire.with in a loop and Resource.mkAcquireType
+                  -- if Pool's withResource is interruptible this calls putResource numThings times
+                      Persist.rawExecute "insert into foobar values(?);"
+                                         [toPersistValue i]
+        )
+
+    -- Concurrent.threadDelay 5000000
+    Monad.void $ Concurrent.killThread threadId
+    putStrLn "killed thread"
