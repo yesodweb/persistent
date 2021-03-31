f61f33f80a471d5e5c95f936f9ce12877f45f43e
Dylan Martin <dmarticus@gmail.com>
2021-03-31 11:30:17 -0700

    whoops don't commit that

--- persistent-postgresql/Database/Persist/query.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO (
-  currencycloud_balances
-) VALUES (
-  externalAccountID, 
-  externalAmount
-) ON CONFLICT DO UPDATE SET (
-  amount = externalAmount
-) WHERE amount != externalAmount
