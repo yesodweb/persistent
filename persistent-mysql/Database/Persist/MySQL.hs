af210b46f9feb73578afbf648e0ad0c73624dc16
Aniket Deshpande <aniketd@users.noreply.github.com>
2021-04-27 22:56:52 +0530

    Expose `openMySQLConn` for when an explicit reference to the connection may be convenient (#1248)
    * Expose internal connection function for persistent-mysql
    
    * Rename exposed fn; add comments
    
    * Add CHANGELOG entry
    
    * Run stylish-haskell on the modified file
    
    * Add @since and bump version
    
    * Update persistent-mysql/ChangeLog.md
    
    Adding a function is a minor version bump - Matt
    
    Co-authored-by: Matt Parsons <parsonsmatt@gmail.com>
    
    * Update persistent-mysql/Database/Persist/MySQL.hs
    
    Adding a function is a minor version bump - Matt
    
    Co-authored-by: Matt Parsons <parsonsmatt@gmail.com>
    
    * Bump version in persistent-mysql.cabal
    
    Co-authored-by: Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
    Co-authored-by: Matt Parsons <parsonsmatt@gmail.com>

--- persistent-mysql/Database/Persist/MySQL.hs
+++ persistent-mysql/Database/Persist/MySQL.hs
@@ -32,36 +32,36 @@ module Database.Persist.MySQL
     , copyUnlessNull
     , copyUnlessEmpty
     , copyUnlessEq
+    , openMySQLConn
     ) where
 
-import qualified Blaze.ByteString.Builder.Char8 as BBB
 import qualified Blaze.ByteString.Builder.ByteString as BBS
+import qualified Blaze.ByteString.Builder.Char8 as BBB
 
 import Control.Arrow
 import Control.Monad
-import Control.Monad.IO.Class (MonadIO (..))
+import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Logger (MonadLoggerIO, runNoLoggingT)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (ExceptT, runExceptT)
-import Control.Monad.Trans.Reader (runReaderT, ReaderT)
+import Control.Monad.Trans.Reader (ReaderT, runReaderT)
 import Control.Monad.Trans.Writer (runWriterT)
 
-import GHC.Stack
-import Data.Conduit
-import qualified Data.Conduit.List as CL
 import Data.Acquire (Acquire, mkAcquire, with)
 import Data.Aeson
 import Data.Aeson.Types (modifyFailure)
 import Data.ByteString (ByteString)
+import Data.Conduit
+import qualified Data.Conduit.List as CL
 import Data.Either (partitionEithers)
 import Data.Fixed (Pico)
 import Data.Function (on)
-import Data.Int (Int64)
 import Data.IORef
-import Data.List (find, intercalate, sort, groupBy)
+import Data.Int (Int64)
+import Data.List (find, groupBy, intercalate, sort)
 import qualified Data.Map as Map
-import Data.Maybe (listToMaybe, mapMaybe, fromMaybe)
+import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
 import Data.Monoid ((<>))
 import qualified Data.Monoid as Monoid
 import Data.Pool (Pool)
@@ -69,18 +69,19 @@ import Data.Text (Text, pack)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.IO as T
+import GHC.Stack
 import System.Environment (getEnvironment)
 
 import Database.Persist.Sql
 import Database.Persist.Sql.Types.Internal (makeIsolationLevelStatement)
 import qualified Database.Persist.Sql.Util as Util
 
-import qualified Database.MySQL.Base          as MySQLBase
-import qualified Database.MySQL.Base.Types    as MySQLBase
-import qualified Database.MySQL.Simple        as MySQL
-import qualified Database.MySQL.Simple.Param  as MySQL
+import qualified Database.MySQL.Base as MySQLBase
+import qualified Database.MySQL.Base.Types as MySQLBase
+import qualified Database.MySQL.Simple as MySQL
+import qualified Database.MySQL.Simple.Param as MySQL
 import qualified Database.MySQL.Simple.Result as MySQL
-import qualified Database.MySQL.Simple.Types  as MySQL
+import qualified Database.MySQL.Simple.Types as MySQL
 
 -- | Create a MySQL connection pool and run the given action.
 -- The pool is properly released after the action finishes using
@@ -120,14 +121,17 @@ withMySQLConn :: (MonadUnliftIO m, MonadLoggerIO m)
 withMySQLConn = withSqlConn . open'
 
 
--- | Internal function that opens a connection to the MySQL
--- server.
-open' :: MySQL.ConnectInfo -> LogFunc -> IO SqlBackend
-open' ci logFunc = do
+-- | Open a connection to MySQL server, initialize the 'SqlBackend' and return
+-- their tuple
+--
+-- @since 2.12.1.0
+openMySQLConn :: MySQL.ConnectInfo -> LogFunc -> IO (MySQL.Connection, SqlBackend)
+openMySQLConn ci logFunc = do
     conn <- MySQL.connect ci
     MySQLBase.autocommit conn False -- disable autocommit!
     smap <- newIORef $ Map.empty
-    return $ SqlBackend
+    let
+      backend = SqlBackend
         { connPrepare    = prepare' conn
         , connStmtMap    = smap
         , connInsertSql  = insertSql'
@@ -153,6 +157,13 @@ open' ci logFunc = do
         , connMaxParams = Nothing
         , connRepsertManySql = Just repsertManySql
         }
+    pure (conn, backend)
+
+
+-- | Internal function that opens a connection to the MySQL server.
+open' :: MySQL.ConnectInfo -> LogFunc -> IO SqlBackend
+open' ci logFunc = snd <$> openMySQLConn ci logFunc
+
 
 -- | Prepare a query.  We don't support prepared statements, but
 -- we'll do some client-side preprocessing here.
