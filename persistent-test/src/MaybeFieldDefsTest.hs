1ff86c50768ef08f1fb3067e950f1db6696a5b38
parsonsmatt <parsonsmatt@gmail.com>
2021-06-29 07:35:56 -0600

    gotta fix that

--- persistent-test/src/MaybeFieldDefsTest.hs
+++ persistent-test/src/MaybeFieldDefsTest.hs
@@ -8,14 +8,14 @@ import Data.String (IsString)
 
 import Init
 
-share [mkPersist sqlSettings,  mkMigrate "maybeFieldDefMigrate"] [persistLowerCase|
+share [mkPersist sqlSettings { mpsGeneric = True },  mkMigrate "maybeFieldDefMigrate"] [persistLowerCase|
   MaybeFieldDefEntity
     optionalString    (Maybe String)
     optionalInt       (Maybe Int)
     deriving Eq Show
 |]
 
-specsWith :: Runner SqlBackend m => RunDb SqlBackend m -> Spec
+specsWith :: (Runner backend m) => RunDb backend m -> Spec
 specsWith runDb = describe "Maybe Field Definitions" $ do
   it "runs appropriate migrations" $ runDb $ do
     emptyEntity <- insert $ MaybeFieldDefEntity Nothing Nothing
