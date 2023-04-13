af7183bffd9cca31bdff7e5f10de42c6e9dc076b
Riuga <riuga@tuta.io>
2023-04-13 05:19:58 -0500

    Update test case
    Check for idempotency of collate= migrations

--- persistent-test/src/MigrationColumnLengthTest.hs
+++ persistent-test/src/MigrationColumnLengthTest.hs
@@ -10,10 +10,11 @@ share [mkPersist sqlSettings, mkMigrate "migration"] [persistLowerCase|
 VaryingLengths
     field1 Int
     field2 T.Text sqltype=varchar(5)
+    field3 T.Text collate=en_US.utf8
 |]
 
 specsWith :: MonadIO m => RunDb SqlBackend m -> Spec
-specsWith runDb =
+specsWith runDb = do
   it "is idempotent" $ runDb $ do
       again <- getMigration migration
       liftIO $ again @?= []
