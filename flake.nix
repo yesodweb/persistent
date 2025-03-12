efb597a27ea14ac74abfa7bd14d15ee666dc6c79
ncaq <ncaq@ncaq.net>
2025-03-12 14:31:55 +0900

    chore: replace flake.nix by haskell.nix
    It is by far simpler to write using haskell.nix.
    It is because it automatically draws an analogy from cabal files and so on.
    There is no need to be afraid of increasing dependencies if using `flake.nix`,
    even if it is `default.nix` which is referenced by other nix projects.
    And upgrade nix GHC version 9.6.6.

--- flake.nix
+++ flake.nix
@@ -3,101 +3,47 @@
     "Persistence interface for Haskell allowing multiple storage methods.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      ghcVer = "ghc902";
-      makeHaskellOverlay = overlay: final: prev: {
-        haskell = prev.haskell // {
-          packages = prev.haskell.packages // {
-            ${ghcVer} = prev.haskell.packages."${ghcVer}".override (oldArgs: {
-              overrides =
-                prev.lib.composeExtensions (oldArgs.overrides or (_: _: { }))
-                (overlay prev);
-            });
-          };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
         };
-      };
-
-      out = system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-            config.allowBroken = true;
-          };
-
-        in {
-          packages = rec {
-            default = persistent;
-            inherit (pkgs.haskell.packages.${ghcVer})
-              persistent persistent-qq persistent-template persistent-mysql
-              persistent-test persistent-postgresql persistent-sqlite;
-          };
-
-          checks = self.packages.${system};
-
-          # for debugging
-          # inherit pkgs;
-
-          devShells.default =
-            let haskellPackages = pkgs.haskell.packages.${ghcVer};
-            in haskellPackages.shellFor {
-              packages = p:
-                with pkgs.haskell.packages.${ghcVer}; [
-                  persistent
-                  persistent-qq
-                  persistent-template
-                  persistent-redis
-                  persistent-mongoDB
-                  persistent-mysql
-                  persistent-test
-                  persistent-postgresql
-                  persistent-sqlite
-                ];
-              withHoogle = true;
-              buildInputs = with haskellPackages;
-                [ haskell-language-server fourmolu ghcid cabal-install ]
-                ++ (with pkgs; [ sqlite ]);
-              # Change the prompt to show that you are in a devShell
-              # shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            project = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc966";
+              shell = {
+                tools = {
+                  cabal = "latest";
+                  cabal-install = "latest";
+                  fourmolu = "latest";
+                  ghcid = "latest";
+                  haskell-language-server = "latest";
+                };
+                buildInputs = with pkgs; [ sqlite ];
+              };
             };
-        };
-    in flake-utils.lib.eachDefaultSystem out // {
-      # this stuff is *not* per-system
-      overlays = {
-        default = makeHaskellOverlay (prev: hfinal: hprev:
-          let
-            hlib = prev.haskell.lib;
-            # this makeLocal business is rather a hack to deal with keeping the
-            # overrides from nixpkgs for these packages for e.g.
-            # persistent-sqlite using the nix-provided sqlite.
-            makeLocal = n: hlib.overrideSrc hprev.${n} { src = ./. + "/${n}"; };
-          in {
-            persistent = makeLocal "persistent";
-            persistent-test = makeLocal "persistent-test";
-            persistent-sqlite = makeLocal "persistent-sqlite";
-            persistent-postgresql = makeLocal "persistent-postgresql";
-            persistent-mysql = makeLocal "persistent-mysql";
-            persistent-redis = makeLocal "persistent-redis";
-            persistent-mongoDB = makeLocal "persistent-mongoDB";
-            persistent-qq = makeLocal "persistent-qq";
-            persistent-template = makeLocal "persistent-template";
-          });
-      };
-    };
+          })
+        ];
+        flake = pkgs.project.flake { };
+      in flake);
 
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org/"
       "https://cache.iog.io" # use GHC binary cache.
     ];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
     allow-import-from-derivation = "true";
   };
 }
