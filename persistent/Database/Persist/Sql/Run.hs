952efa9368acb6c1be68edb48aeef701465b3058
Andrey Prokopenko <andrey@chordify.net>
2026-08-26 12:46:01 +0200

    fix: make `loggedClose` uninterruptible

--- persistent/Database/Persist/Sql/Run.hs
+++ persistent/Database/Persist/Sql/Run.hs
@@ -280,11 +280,11 @@ createSqlPoolWithConfig
     -> m (Pool backend)
 createSqlPoolWithConfig mkConn config = do
     logFunc <- askLoggerIO
-    -- Resource pool will swallow any exceptions from close. We want to log
-    -- them instead.
+    -- NOTE: resource-pool >= 0.5 no longer runs the pool's free action
+    -- uninterruptibly, and no longer swallows its exceptions.
     let
         loggedClose :: backend -> IO ()
-        loggedClose backend =
+        loggedClose backend = UE.uninterruptibleMask_ $
             close' backend `UE.catchAny` \e -> do
                 runLoggingT
                     (logError $ T.pack $ "Error closing database connection in pool: " ++ show e)
